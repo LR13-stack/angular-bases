15981af3b6a3afebc096b7f99221a93106ab2cd1
Luis Romero Pretel <marco2002rp@gmail.com>
2024-11-15 10:23:23 -0500

    section 05 -- add DBZ character management features with add and list components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterModule } from "./counter/counter.module";
 import { HeroesModule } from "./heroes/heroes.module";
+import { DbzModule } from "./dbz/dbz.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterModule, HeroesModule],
+  imports: [RouterOutlet, CounterModule, HeroesModule, DbzModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
