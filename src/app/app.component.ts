5d55c2a73f7cd27b52cb62a6e8f3311079c154c3
Luis Romero Pretel <marco2002rp@gmail.com>
2024-11-15 10:03:54 -0500

    Add hero and counter components with styles and templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterModule } from "./counter/counter.module";
+import { HeroesModule } from "./heroes/heroes.module";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterModule, HeroesModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
